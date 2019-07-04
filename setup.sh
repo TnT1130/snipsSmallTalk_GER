d2b34e19cef0727406086562c7ec4b9c36eab5e3
xinQuon <51043621+xinQuon@users.noreply.github.com>
2019-07-04 19:12:50 +0200

    Update setup.sh
    copy paste von openhab

--- setup.sh
+++ setup.sh
@@ -1,26 +1,51 @@
-#/usr/bin/env bash -e
+#!/usr/bin/env bash -e
 
-if [ ! -e "./config.ini" ]
+
+
+# Copy config.ini.default if config.ini doesn't exist.
+
+if [ ! -e config.ini ]
+
 then
+
     cp config.ini.default config.ini
+
 fi
+
 
 
 VENV=venv
 
+
+
 if [ ! -d "$VENV" ]
+
 then
+
+
 
     PYTHON=`which python3`
 
+
+
     if [ ! -f $PYTHON ]
+
     then
+
         echo "could not find python"
+
     fi
+
     virtualenv -p $PYTHON $VENV
+
+
 
 fi
 
+
+
 . $VENV/bin/activate
+
+
 
 pip3 install -U -r requirements.txt
